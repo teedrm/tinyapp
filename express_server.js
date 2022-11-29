1fdbbf551cb22430ba58d25d515883439ad5a80e
Thy Nguyen <tee@Thys-iMac.local>
2022-11-28 21:48:37 -0700

    url shortening - HTTP redirection taking directly to the links in database

--- express_server.js
+++ express_server.js
@@ -24,19 +24,21 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req,res) => {
+    res.render("urls_new");
+})
+
 app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
     res.render("urls_show", templateVars);
 });
 
-app.get("/urls/new", (req,res) => {
-    res.render("urls_new");
-})
+app.get("/u/:id", (req, res) => {
+    const longURL = urlDatabase[req.params.id]
+    res.redirect(longURL);
+});
 
-app.post("/urls", (req, res) => {
-    console.log(req.body); // Log the POST request body to the console
-    res.send("Ok"); // Respond with 'Ok' (we will replace this)
-  });
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
