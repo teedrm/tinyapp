1cb7674c4cf11c1acc595a4230ebc7a6eeccda0c
Thy Nguyen <tee@Thys-iMac.local>
2022-11-29 18:57:32 -0700

    add form that submits an updated longURL/ add Edit buttons and update new link

--- express_server.js
+++ express_server.js
@@ -51,7 +51,13 @@ app.get("/u/:id", (req, res) => {
     res.redirect(longURL);
 });
 
-// DELETING URL
+app.post("/urls/:id", (req, res) => {
+    const shortURL = req.params.id;
+    const longURL = req.body.longURL;
+    urlDatabase[shortURL] = longURL;
+    res.redirect("/urls");
+});
+
 app.post("/urls/:id/delete", (req, res) => {
     delete urlDatabase[req.params.id]
     res.redirect("/urls");
